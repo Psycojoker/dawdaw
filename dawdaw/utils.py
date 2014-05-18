cfd17a2eadafd228934069a33ed95302420b7742
Laurent Peuch <cortex@worlddomination.be>
2014-05-18 09:39:25 +0200

    [enh] add debug utils

--- dawdaw/utils.py
+++ dawdaw/utils.py
@@ -1,13 +1,23 @@
+import yaml
 from .magic import global_state
+
+
+def debug(value=True):
+    global_state["current_state"]["debug"] = value
 
 
 def salt_render(content, localtion, name, env, tmplpath, **kwargs):
     global_state["current_state"] = {
         "name": name,
         "content": {},
+        "debug": False,
     }
 
     # YOLO
     exec(content.read())
+
+    if global_state["current_state"]["debug"]:
+        print "debug:"
+        print yaml.dump(global_state["current_state"]["content"], default_flow_style=False)
 
     return global_state["current_state"]["content"]
